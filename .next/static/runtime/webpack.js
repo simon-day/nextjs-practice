99442b60bd80fe0b02482b0c9c26e297f8372e6e
simon-macbook <simon.day91@hotmail.co.uk>
2020-02-08 16:06:33 +0800

    Add Express and config for custom server side routes

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9d1d80f319a9de579b8a";
+/******/ 	var hotCurrentHash = "492a32f07544fbf26a46";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
